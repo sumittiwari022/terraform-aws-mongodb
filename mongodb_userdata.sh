6882f420b7eacb0313cba5d3f815901aca5f59b4
Sumit Tiwari <60305045+sumittiwari022@users.noreply.github.com>
2023-08-26 23:23:33 +0530

    Update mongodb_userdata.sh

--- mongodb_userdata.sh
+++ mongodb_userdata.sh
@@ -23,7 +23,7 @@ systemctl restart ssh
 # Installing MongoDB and Python with Dependent Packages and Pip
 apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "deb [ arch=arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt-get update
 apt-get install -y mongodb-org unzip python3-distutils jq build-essential python3-dev
 chown -R mongodb:mongodb /var/lib/mongodb
